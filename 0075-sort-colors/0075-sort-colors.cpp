bf7d6934c41d9bb58f8453703c32089d6943ba33
Harini N <harinins07@gmail.com>
2024-09-20 13:14:40 +0530

    Time: 0 ms (100%), Space: 10.3 MB (25.37%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,61 +1,29 @@
 class Solution {
 public:
-
-    void mergesort(vector<int>& nums, int low, int high)
-    {
-        int mid=low+(high-low)/2;
-        if(low<high)
-        {
-            mergesort(nums,low,mid);
-            mergesort(nums,mid+1,high);
-            merge(nums,low,mid,high);
-        }
-        return;
-    }
-
-    void merge(vector<int>& nums,int low,int mid,int high)
-    {
-        vector<int> temp;
-        int left=low;
-        int right=mid+1;
-
-        while(left<=mid&&right<=high)
-        {
-            if(nums[left]<=nums[right])
-            {
-                temp.push_back(nums[left]);
-                left++;
-            }
-            else
-            {
-                temp.push_back(nums[right]);
-                right++;
-            }
-        }
-
-        while(left<=mid)
-        {
-            temp.push_back(nums[left]);
-            left++;
-        }
-
-        while(right<=high)
-        {
-            temp.push_back(nums[right]);
-            right++;
-        }
-
-        for(int i=low;i<=high;i++)
-        {
-            nums[i]=temp[i-low];
-        }
-    }
-
     void sortColors(vector<int>& nums) {
+        //Dutch National Flag Algo
         int low=0;
         int high=nums.size()-1;
-        mergesort(nums,low,high);
+        int mid=0;
+
+        while(mid<=high)
+        {
+            if(nums[mid]==0)
+            {
+                swap(nums[low],nums[mid]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid]==1)
+            {
+                mid++;
+            }
+            else{
+                swap(nums[mid],nums[high]);
+               // mid++;
+                high--;
+            }
+        }
         return;
     }
-
 };
